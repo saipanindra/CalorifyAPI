d4ca70a6c4acb64a1119b4b03c329dafa0d2c93d
Sai Paleti <saipanindra9@gmail.com>
2017-08-27 02:10:14 -0700

    rename to calorify-api

---
+++ calorify-api/controllers/Admins.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var url = require('url');
+
+var Admins = require('./AdminsService');
+
+module.exports.addCuisine = function addCuisine (req, res, next) {
+  Admins.addCuisine(req.swagger.params, res, next);
+};
+
+module.exports.addIngredient = function addIngredient (req, res, next) {
+  Admins.addIngredient(req.swagger.params, res, next);
+};
+
+module.exports.addUser = function addUser (req, res, next) {
+  Admins.addUser(req.swagger.params, res, next);
+};
