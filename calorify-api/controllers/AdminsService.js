1d22196e64903fab520bb1ccb07c8c9fbd7dec82
Sri <srilavanya.paleti@gmail.com>
2019-04-17 23:19:41 -0600

    enforce-lint-rules: Formatted the classes using the webstorm format code.

--- calorify-api/controllers/AdminsService.js
+++ calorify-api/controllers/AdminsService.js
@@ -1,35 +1,35 @@
 'use strict';
 
-exports.addCuisine = function(args, res, next) {
-  /**
-   * adds cuisine to the system
-   * Adds a new cuisine to the system
-   *
-   * cuisine Cuisines Cuisine object to add (optional)
-   * no response value expected for this operation
-   **/
+exports.addCuisine = function (args, res, next) {
+    /**
+     * adds cuisine to the system
+     * Adds a new cuisine to the system
+     *
+     * cuisine Cuisines Cuisine object to add (optional)
+     * no response value expected for this operation
+     **/
     res.end();
 };
 
-exports.addIngredient = function(args, res, next) {
-  /**
-   * adds an ingredient
-   * Adds an ingredient to the system
-   *
-   * ingredient Ingredients Inventory item to add (optional)
-   * no response value expected for this operation
-   **/
+exports.addIngredient = function (args, res, next) {
+    /**
+     * adds an ingredient
+     * Adds an ingredient to the system
+     *
+     * ingredient Ingredients Inventory item to add (optional)
+     * no response value expected for this operation
+     **/
     res.end();
 };
 
-exports.addUser = function(args, res, next) {
-  /**
-   * adds user to the system and create user account
-   * Adds an user to the system
-   *
-   * user Users User object to add (optional)
-   * no response value expected for this operation
-   **/
+exports.addUser = function (args, res, next) {
+    /**
+     * adds user to the system and create user account
+     * Adds an user to the system
+     *
+     * user Users User object to add (optional)
+     * no response value expected for this operation
+     **/
     res.end();
 };
 
--- calorify-api/controllers/DevelopersService.js
+++ calorify-api/controllers/DevelopersService.js
@@ -1,20 +1,20 @@
 'use strict';
 
-exports.getCuisines = function(args, res, next) {
-  /**
-   * get list of cuisines
-   * get list of cuisines in the system.
-   *
-   * type String pass an optional cuisine type for looking up cuisines (optional)
-   * skip Integer number of records to skip for pagination (optional)
-   * limit Integer maximum number of records to return (optional)
-   * returns List
-   **/
+exports.getCuisines = function (args, res, next) {
+    /**
+     * get list of cuisines
+     * get list of cuisines in the system.
+     *
+     * type String pass an optional cuisine type for looking up cuisines (optional)
+     * skip Integer number of records to skip for pagination (optional)
+     * limit Integer maximum number of records to return (optional)
+     * returns List
+     **/
     var examples = {};
-    examples['application/json'] = [ {
-        'Id' : 'd290f1ee-6c54-4b01-90e6-d701748f0851',
-        'Name' : 'Asian'
-    } ];
+    examples['application/json'] = [{
+        'Id': 'd290f1ee-6c54-4b01-90e6-d701748f0851',
+        'Name': 'Asian'
+    }];
     if (Object.keys(examples).length > 0) {
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
@@ -23,24 +23,24 @@ exports.getCuisines = function(args, res, next) {
     }
 };
 
-exports.getUsers = function(args, res, next) {
-  /**
-   * get list of users
-   * get list of users in the system.
-   *
-   * username String pass an optional user name(first name or last name) string for looking up users (optional)
-   * skip Integer number of records to skip for pagination (optional)
-   * limit Integer maximum number of records to return (optional)
-   * returns List
-   **/
+exports.getUsers = function (args, res, next) {
+    /**
+     * get list of users
+     * get list of users in the system.
+     *
+     * username String pass an optional user name(first name or last name) string for looking up users (optional)
+     * skip Integer number of records to skip for pagination (optional)
+     * limit Integer maximum number of records to return (optional)
+     * returns List
+     **/
     var examples = {};
-    examples['application/json'] = [ {
-        'DateOfBirth' : '1st Jan 2017',
-        'FirstName' : 'Christopher',
-        'Sex' : 'Male',
-        'Id' : 'd290f1ee-6c54-4b01-90e6-d701748f0851',
-        'LastName' : 'Nolan'
-    } ];
+    examples['application/json'] = [{
+        'DateOfBirth': '1st Jan 2017',
+        'FirstName': 'Christopher',
+        'Sex': 'Male',
+        'Id': 'd290f1ee-6c54-4b01-90e6-d701748f0851',
+        'LastName': 'Nolan'
+    }];
     if (Object.keys(examples).length > 0) {
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
@@ -49,26 +49,26 @@ exports.getUsers = function(args, res, next) {
     }
 };
 
-exports.searchInventory = function(args, res, next) {
-  /**
-   * gets List of Ingredients
-   * By passing in the appropriate options, you can search for available ingredients in the system 
-   *
-   * searchString String pass an optional search string for looking up ingredients (optional)
-   * skip Integer number of records to skip for pagination (optional)
-   * limit Integer maximum number of records to return (optional)
-   * returns List
-   **/
+exports.searchInventory = function (args, res, next) {
+    /**
+     * gets List of Ingredients
+     * By passing in the appropriate options, you can search for available ingredients in the system
+     *
+     * searchString String pass an optional search string for looking up ingredients (optional)
+     * skip Integer number of records to skip for pagination (optional)
+     * limit Integer maximum number of records to return (optional)
+     * returns List
+     **/
     var examples = {};
-    examples['application/json'] = [ {
-        'CategoryId' : 'ed90f1ee-6c54-4b01-90e6-d701748f0851',
-        'NutritionId' : 'xd90f1eed-6c54-4b01-90e6-d701748f0851',
-        'IngredientId' : 'd290f1ee-6c54-4b01-90e6-d701748f0851',
-        'Color' : 'White',
-        'Unit' : 'grams',
-        'AvailableQuantity' : 30,
-        'Name' : 'Salt'
-    } ];
+    examples['application/json'] = [{
+        'CategoryId': 'ed90f1ee-6c54-4b01-90e6-d701748f0851',
+        'NutritionId': 'xd90f1eed-6c54-4b01-90e6-d701748f0851',
+        'IngredientId': 'd290f1ee-6c54-4b01-90e6-d701748f0851',
+        'Color': 'White',
+        'Unit': 'grams',
+        'AvailableQuantity': 30,
+        'Name': 'Salt'
+    }];
     if (Object.keys(examples).length > 0) {
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
